367902926e4220716c69b8035098803b58ac34cf
zhengchao <524843954@qq.com>
2022-05-19 11:16:54 +0800

    富文本处理

--- pages/richText.vue
+++ pages/richText.vue
@@ -13,7 +13,7 @@
     const eventChannel = ctx.getOpenerEventChannel()
     // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据
     eventChannel.once('acceptRichText', (data) => {
-      richText.value = data?.replace(/<p><\/p>/g,"<br/>")
+      richText.value = data?.replace(/<p><\/p>/g,"<br/>").replace(/<img/g, `<img style="max-width:100%;display:block"`)
     })
   })
 </script>
